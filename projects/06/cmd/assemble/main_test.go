0938275a29544e148c14da57f2df35f8f98026e4
nayuta-ai <nayuta723@gmail.com>
2022-10-03 14:12:23 +0900

    modify benchmark test

--- projects/06/cmd/assemble/main_test.go
+++ projects/06/cmd/assemble/main_test.go
@@ -1,9 +1,16 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func BenchmarkMain(b *testing.B) {
 	for i := 0; i < 2; i++ {
-		main()
+		filepath := "test/add/Add"
+		fp, _ := os.Open(filepath + ".asm")
+		defer fp.Close()
+		byteInstraction, _ := scanline(fp)
+		_ = createHack(byteInstraction, filepath)
 	}
 }
